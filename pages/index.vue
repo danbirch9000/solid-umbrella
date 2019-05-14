dad4744f6dedde2e5eca2d7fe6ea7194505cfd66
Dan <danny_birch@hotmail.com>
2019-05-14 19:38:39 +0100

    Removal of dialog example

--- pages/index.vue
+++ pages/index.vue
@@ -10,40 +10,6 @@
         savings over time.
       </p>
       <NewGoal :allow-save="false" />
-      <div class="text-xs-center">
-        <v-dialog v-model="dialog" width="500">
-          <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark v-on="on">
-              Click Me
-            </v-btn>
-          </template>
-
-          <v-card>
-            <v-card-title class="headline grey lighten-2" primary-title>
-              Privacy Policy
-            </v-card-title>
-
-            <v-card-text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </v-card-text>
-
-            <v-divider />
-
-            <v-card-actions>
-              <v-spacer />
-              <v-btn color="primary" flat @click="dialog = false">
-                I accept
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </div>
     </section>
   </section>
 </template>
@@ -53,15 +19,7 @@ import Login from "~/components/Login";
 import NewGoal from "~/components/goals/NewGoal";
 
 export default {
-  components: { NewGoal, Login },
-  data() {
-    return {
-      dialog: false
-    };
-  },
-  beforeMount() {
-    this.$store.commit("CLOSE_MENU");
-  }
+  components: { NewGoal, Login }
 };
 </script>
 
